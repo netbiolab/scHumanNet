e7e4ca5a5376cd38bf4cc507445566d63b4df2d7
JunhaCha <junhacha@yonsei.ac.kr>
2022-05-12 15:45:42 -0400

    change documentation

--- R/FindAllHub.R
+++ R/FindAllHub.R
@@ -1,7 +1,7 @@
 #' Find statistically significant hub genes in each scHumanNets
 #'
 #' @param net.list output of SortAddLLS. list of network dataframe
-#' @param centrality default is degree centrality. Available options are: c("degree", "betweenness","closeness", "eigenvector") degree is calculated with sum of weights. Weights are uniform in betweenness centrality.
+#' @param centrality default is degree centrality. Available options are, c("degree", "betweenness","closeness", "eigenvector") degree is calculated with sum of weights. Weights are uniform in betweenness centrality.
 #' @param q.method default BH, input one of the following c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none")
 #' @param threshold a threshold value to cut the significant hub genes. default 0.05
 #'
